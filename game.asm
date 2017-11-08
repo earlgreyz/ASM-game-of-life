39f6ff10c9f91e73ce8c493fe456b5b92a9d6fd1
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2017-11-09 00:51:05 +0100

    Implement count neighbours function

--- game.asm
+++ game.asm
@@ -14,5 +14,58 @@ start:
   mov [map], r8     ; third argument
   ret
 
+;; Gets the pointer to the requested map cell.
+;; @param x (ecx)
+;; @param y (edx)
+;; @returns pointer to map[x][y] in RAX
+_map_get:
+  ;; offset = width * y + x
+  mov eax, [width]
+  mul rax, edx
+  add rax, ecx
+  ;; cell_ptr = map + 4 * offset
+  mov rdx, [map]
+  lea rax, [rdx + 4 * rax]
+  ret
+
+;; Counts the number of alive cell neighbours
+;; @param x (ecx)
+;; @param y (edx)
+;; @returns void
+_count_neighbours:
+  mov r8d, ecx   ; Save original x
+  mov r9d, edx   ; Save original y
+  call _map_get  ; _map_get(x, y)
+  mov r15d, rax  ; Save address of map[x][y]
+
+  xor r14d, r14d ; Set neighbours = 0
+  mov r11d, -1   ; Initialize y_counter
+_loop_y:
+  mov r10d, -1   ; Initialize x_counter
+_loop_x:
+  ;; cell_ptr = _map_get(x + x_couter, y + y_counter)
+  mov ecx, r8d
+  add ecx, r10d
+  mov edx, r9d
+  add edx, r11d
+  call _map_get
+  ;; if (*cell_ptr == 0) end loop
+  mov ecx, [rax]
+  jecxz _loop_end
+  ;; else increase alive neighbours count
+  inc r14d
+_loop_end:
+  ;; Check x loop
+  inc r10d
+  cmp r10d, 1
+  jle _loop_x
+  ;; Check y loop
+  inc r11d
+  cmp r11d, 1
+  jle _loop_y
+  ;; Return
+  mov eax, r14d
+  ret
+
 run:
   ret
